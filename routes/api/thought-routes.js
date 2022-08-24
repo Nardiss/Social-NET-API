203a0436d3cb3e4f9262839d68f19c1640bdc0d9
unknown <nardissoloab@gmail.com>
2022-08-24 00:36:07 -0500

    edited routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllThoughts, getThoughtById, addThought, updateThought, deleteThought, addReaction, deleteReaction } = require('../../Social-NET-API/controllers/thought-controller');
+const { getAllThoughts, getThoughtById, addThought, updateThought, deleteThought, addReaction, deleteReaction } = require('../../controllers/thought-controller');
 
 router.route('/')
     .get(getAllThoughts);
